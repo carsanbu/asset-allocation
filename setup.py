5037bcf859a1a611d0ed0d0aec6db4426c3d4654
Carlos Sanmartín Bustos <carsanbu@entramado.net>
2021-05-30 13:28:58 +0200

    feat: python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='asset_allocation',
+      version='0.1',
+      description='Asset allocation tools written in python',
+      url='https://github.com/carsanbu/asset-allocation',
+      author='Carlos Sanmartín',
+      author_email='carsanbu@gmail.com',
+      license='GPL v3.0',
+      packages=['asset_allocation'],
+      zip_safe=False)
+
